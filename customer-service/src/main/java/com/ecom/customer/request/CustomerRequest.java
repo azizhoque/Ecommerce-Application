f655991353c899971a4b9fecb6659b76c512cdeb
azizhoque <azizhoque72@gmail.com>
2026-09-20 19:05:12 +0530

    add customer request class

---
+++ customer-service/src/main/java/com/ecom/customer/request/CustomerRequest.java
@@ -0,0 +1,19 @@
+package com.ecom.customer.request;
+
+import com.ecom.model.Address;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+
+public class CustomerRequest {
+
+	@NotNull(message = "Customer First Name is required")
+	String firstName;
+	@NotNull(message = "Customer Last Name is required")
+	String lastName;
+	@NotNull(message = "Customer email is required")
+	@Email(message = "Customer email is not a valid email address")
+	String email;
+
+	Address address;
+}
