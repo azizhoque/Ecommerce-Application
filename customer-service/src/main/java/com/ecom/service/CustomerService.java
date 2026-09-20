b8f23dcfda24257867ab017e6d42995721703ff6
azizhoque <azizhoque72@gmail.com>
2026-09-21 02:39:27 +0530

    Update customer exception handling

--- customer-service/src/main/java/com/ecom/service/CustomerService.java
+++ customer-service/src/main/java/com/ecom/service/CustomerService.java
@@ -3,7 +3,9 @@ package com.ecom.service;
 import org.springframework.stereotype.Service;
 
 import com.ecom.customer.request.CustomerRequest;
+import com.ecom.exceptionhandle.CustomerNotFoundException;
 import com.ecom.mapper.CustomerMapper;
+import com.ecom.model.Customer;
 import com.ecom.repository.CustomerRepository;
 
 import jakarta.validation.Valid;
@@ -22,4 +24,15 @@ public class CustomerService {
 		return customer.getId();
 	}
 
+	public void updateCustomer(String id, @Valid CustomerRequest request) {
+
+		var customer = repository.findById(id)
+				.orElseThrow(() -> new CustomerNotFoundException("Customer Not found with id: " + id));
+		customer.setFirstName(request.getFirstName());
+		customer.setLastName(request.getLastName());
+		customer.setEmail(request.getEmail());
+		customer.setAddress(request.getAddress());
+
+		repository.save(customer);
+	}
 }
