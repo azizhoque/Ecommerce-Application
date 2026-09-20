69ffbb55f353c7a3d6a88c2ddef5cc6106759d3a
azizhoque <azizhoque72@gmail.com>
2026-09-21 03:42:02 +0530

    add get end point in controller

--- customer-service/src/main/java/com/ecom/controller/CustomerController.java
+++ customer-service/src/main/java/com/ecom/controller/CustomerController.java
@@ -1,7 +1,10 @@
 package com.ecom.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecom.customer.request.CustomerRequest;
+import com.ecom.customer.response.CustomerResponse;
 import com.ecom.service.CustomerService;
 
 import jakarta.validation.Valid;
@@ -32,6 +36,11 @@ public class CustomerController {
 	public ResponseEntity<Void> updateCustomer(@PathVariable String id, @RequestBody @Valid CustomerRequest request){
 		service.updateCustomer(id, request);
 		return new ResponseEntity<Void>(HttpStatus.ACCEPTED);
-		
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<CustomerResponse>> findAll(){
+		var list = service.findAllCustomer();
+		return new ResponseEntity<List<CustomerResponse>>(list,HttpStatus.OK);
 	}
 }
