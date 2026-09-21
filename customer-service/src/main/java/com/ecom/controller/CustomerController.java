ffd9ddc4c95434380c49d582b3af83b240684d9b
azizhoque <azizhoque72@gmail.com>
2026-09-21 16:00:17 +0530

    add needed end point in controller class

--- customer-service/src/main/java/com/ecom/controller/CustomerController.java
+++ customer-service/src/main/java/com/ecom/controller/CustomerController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,14 +34,32 @@ public class CustomerController {
 	}
 	
 	@PutMapping("/updateCustomer/{id}")
-	public ResponseEntity<Void> updateCustomer(@PathVariable String id, @RequestBody @Valid CustomerRequest request){
-		service.updateCustomer(id, request);
-		return new ResponseEntity<Void>(HttpStatus.ACCEPTED);
+	public ResponseEntity<String> updateCustomer(@PathVariable String id, @RequestBody @Valid CustomerRequest request){
+		String status = service.updateCustomer(id, request);
+		return new ResponseEntity<String>(status,HttpStatus.ACCEPTED);
 	}
 	
 	@GetMapping
 	public ResponseEntity<List<CustomerResponse>> findAll(){
 		var list = service.findAllCustomer();
 		return new ResponseEntity<List<CustomerResponse>>(list,HttpStatus.OK);
+	}
+	
+	@GetMapping("/exist/{customerId}")
+	public ResponseEntity<Boolean> existCustomerById(@PathVariable String customerId){
+		var boolean1 = service.existById(customerId);
+		return new ResponseEntity<Boolean>(boolean1,HttpStatus.FOUND);
+	}
+	
+	@GetMapping("/find/{customerId}")
+	public ResponseEntity<CustomerResponse> findCustomerById(@PathVariable String customerId){
+		CustomerResponse response = service.findById(customerId);
+		return new ResponseEntity<CustomerResponse>(response,HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/deleteCustomer/{customerId}")
+	public ResponseEntity<String> deleteCustomerById(@PathVariable String customerId){
+		var status = service.deleteById(customerId);
+		return new ResponseEntity<String>(status,HttpStatus.OK);
 	}
 }
