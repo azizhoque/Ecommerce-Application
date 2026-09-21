5732e01ce87f06bfcbf2797150c083e8aaadbc79
azizhoque <azizhoque72@gmail.com>
2026-09-22 00:45:31 +0530

    add required method in repository class

--- product-service/src/main/java/com/ecom/repository/IProductRepository.java
+++ product-service/src/main/java/com/ecom/repository/IProductRepository.java
@@ -1,5 +1,7 @@
 package com.ecom.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.ecom.model.Product;
 
 @Repository
 public interface IProductRepository extends JpaRepository<Product, Integer> {
+
+	List<Product> findAllByIdOrderById(List<Integer> productIds);
 
 }
