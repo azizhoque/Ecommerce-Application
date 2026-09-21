1355bd17c8205198b5439b957baca13502736de7
azizhoque <azizhoque72@gmail.com>
2026-09-22 02:47:51 +0530

    made small change in product class

--- product-service/src/main/java/com/ecom/model/Product.java
+++ product-service/src/main/java/com/ecom/model/Product.java
@@ -23,7 +23,7 @@ import lombok.Setter;
 public class Product {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
 	private String description;
