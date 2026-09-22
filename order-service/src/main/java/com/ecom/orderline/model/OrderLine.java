c91c544c47d187e19f4663f1dc8bbaddffe7a264
azizhoque <azizhoque72@gmail.com>
2026-09-22 12:11:04 +0530

    add order-line and payment method class and made small changes in order class

---
+++ order-service/src/main/java/com/ecom/orderline/model/OrderLine.java
@@ -0,0 +1,37 @@
+package com.ecom.orderline.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.ecom.order.model.Order;
+import com.ecom.order.model.PaymentMethod;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Builder
+@Entity
+public class OrderLine {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+	@ManyToOne
+	@JoinColumn(name = "order_id")
+	private Order order;
+	private Integer productId;
+	private double quantity;
+}
