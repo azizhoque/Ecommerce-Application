67d50398250bde627c3901e40609a2423ef2bb8e
azizhoque <azizhoque72@gmail.com>
2026-09-22 12:26:31 +0530

    add order-request class

---
+++ order-service/src/main/java/com/ecom/order/request/OrderRequest.java
@@ -0,0 +1,32 @@
+package com.ecom.order.request;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import com.ecom.order.model.PaymentMethod;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+public record OrderRequest(
+		Integer id,
+		String reference,
+		@Positive(message = "Order amount should be positive")
+		BigDecimal amount,
+		
+		@NotNull(message = "Payment method should be precised")
+		PaymentMethod payment,
+		
+		@NotNull(message = "Customer should be present")
+		@NotBlank(message = "Customer should be present")
+		@NotEmpty(message = "Customer should be present")
+		String customerId,
+		
+		@NotEmpty( message = "You should be purchase at least one product")
+		List<PurchaseRequest> product
+		
+		) {
+
+}
