9309b49298d4ea3755e4ab3f9359f3045891f1d1
azizhoque <azizhoque72@gmail.com>
2026-09-22 13:00:40 +0530

    add client to get customer details

---
+++ order-service/src/main/java/com/ecom/order/client/CustomerClient.java
@@ -0,0 +1,15 @@
+package com.ecom.order.client;
+
+import java.util.Optional;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "customer-service",
+             url = "${application.config.customer-url}")
+public interface CustomerClient {
+
+	@GetMapping("/find/{customerId}")
+	Optional<CustomerResponse> findCustomerByID(@PathVariable String customerId);
+}
