86400cb797ea9e852f0dfd29dc86faf9fb2fbc94
azizhoque <azizhoque72@gmail.com>
2026-09-23 16:25:13 +0530

    add payment-service also add payment entity

---
+++ payment-service/src/main/java/com/ecom/payment/model/Payment.java
@@ -0,0 +1,52 @@
+package com.ecom.payment.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.validation.annotation.Validated;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Validated
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "payment")
+public class Payment {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	private BigDecimal amount;
+	
+	@Enumerated(EnumType.STRING)
+	private PaymentMethod payMethod;
+	
+	private Integer OrderId;
+	
+	@CreatedDate
+	@Column(updatable = false, nullable = false)
+	private LocalDateTime createDate;
+	
+	@LastModifiedDate
+	@Column(insertable = false)
+	private LocalDateTime lastModifiedDate;
+}
---
+++ payment-service/src/main/java/com/ecom/payment/model/PaymentMethod.java
@@ -0,0 +1,11 @@
+package com.ecom.payment.model;
+
+public enum PaymentMethod {
+
+	CREDIT_CARD,
+	
+	DEBIT_CARD,
+	
+	UPI
+	
+}
