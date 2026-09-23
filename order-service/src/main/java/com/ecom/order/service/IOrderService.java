a7222081a70a6bf7975ca289fac09391099c90e7
azizhoque <azizhoque72@gmail.com>
2026-09-23 15:32:00 +0530

    add method to get list of order

--- order-service/src/main/java/com/ecom/order/service/IOrderService.java
+++ order-service/src/main/java/com/ecom/order/service/IOrderService.java
@@ -1,9 +1,14 @@
 package com.ecom.order.service;
 
+import java.util.List;
+
 import com.ecom.order.request.OrderRequest;
+import com.ecom.order.response.OrderResponse;
 
 public interface IOrderService {
 
 	public Integer createOrder(OrderRequest request);
+
+	public List<OrderResponse> findAll();
 
 }
