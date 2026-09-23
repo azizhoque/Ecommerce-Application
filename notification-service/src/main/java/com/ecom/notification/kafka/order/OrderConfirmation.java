4d9cad96e288b6f4cbaed2b1d52feb9a0fdee920
azizhoque <azizhoque72@gmail.com>
2026-09-24 01:24:21 +0530

    add order-confiamrtion record

---
+++ notification-service/src/main/java/com/ecom/notification/kafka/order/OrderConfirmation.java
@@ -0,0 +1,22 @@
+package com.ecom.notification.kafka.order;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import com.ecom.notification.kafka.payment.PaymentMethod;
+
+public record OrderConfirmation(
+		
+		String orderReference,
+		
+		BigDecimal amount,
+		
+		PaymentMethod payMethod,
+		
+		Customer customer,
+		
+		List<product> product
+		
+		) {
+
+}
