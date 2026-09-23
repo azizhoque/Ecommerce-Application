d630b4fb61b6b9a837f9226a700f36d89105e320
azizhoque <azizhoque72@gmail.com>
2026-09-23 17:30:12 +0530

    implement send notification logic

--- payment-service/src/main/java/com/ecom/payment/notification/NotificationProducer.java
+++ payment-service/src/main/java/com/ecom/payment/notification/NotificationProducer.java
@@ -1,6 +1,9 @@
 package com.ecom.payment.notification;
 
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
 import com.ecom.payment.notification.request.PaymentNotificationrequest;
@@ -14,4 +17,17 @@ import lombok.extern.slf4j.Slf4j;
 public class NotificationProducer {
 
 	private KafkaTemplate<String, PaymentNotificationrequest> kafkaTemplate;
+	
+	public void sendNotification(PaymentNotificationrequest request) {
+		
+		log.info("Sending notification with body <{}> ", request);
+		
+		Message<PaymentNotificationrequest> message = 
+				MessageBuilder.
+				withPayload(request).
+				setHeader(KafkaHeaders.TOPIC, "payment-topic").
+				build();
+		
+		kafkaTemplate.send(message);
+	}
 }
