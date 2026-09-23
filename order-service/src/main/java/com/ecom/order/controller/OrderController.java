d51d213bbce04d30b5faf2d1e7d3f326063f36d4
azizhoque <azizhoque72@gmail.com>
2026-09-23 16:04:11 +0530

    create api end point to fetch order

--- order-service/src/main/java/com/ecom/order/controller/OrderController.java
+++ order-service/src/main/java/com/ecom/order/controller/OrderController.java
@@ -1,13 +1,18 @@
 package com.ecom.order.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecom.order.request.OrderRequest;
+import com.ecom.order.response.OrderResponse;
 import com.ecom.order.service.IOrderService;
 
 import jakarta.validation.Valid;
@@ -24,5 +29,15 @@ public class OrderController {
 	public ResponseEntity<Integer> createOrder(@RequestBody @Valid OrderRequest request){
 		service.createOrder(request);
 		return new ResponseEntity<Integer>(HttpStatus.CREATED);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<OrderResponse>> findAll(){
+		return new ResponseEntity<List<OrderResponse>>(service.findAll(),HttpStatus.OK);
+	}
+	
+	@GetMapping("/{orderId}")
+	public ResponseEntity<OrderResponse> findByOrderId(@PathVariable Integer orderId){
+		return new ResponseEntity<OrderResponse>(service.findById(orderId),HttpStatus.OK);
 	}
 }
