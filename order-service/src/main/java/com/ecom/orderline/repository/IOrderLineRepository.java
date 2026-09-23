47daed4e5ebf554062a18206bed1495e73cab1d8
azizhoque <azizhoque72@gmail.com>
2026-09-23 16:01:37 +0530

    add custom method to get all orderLine using order-id

--- order-service/src/main/java/com/ecom/orderline/repository/IOrderLineRepository.java
+++ order-service/src/main/java/com/ecom/orderline/repository/IOrderLineRepository.java
@@ -1,5 +1,7 @@
 package com.ecom.orderline.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.ecom.orderline.model.OrderLine;
 
 @Repository
 public interface IOrderLineRepository extends JpaRepository<OrderLine, Integer> {
+
+	public List<OrderLine> findAllByOrderId(Integer orderId);
 
 }
