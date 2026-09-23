e178ee85cf55b6ad9abc7c08b60ffc589c2a1c0b
azizhoque <azizhoque72@gmail.com>
2026-09-23 15:31:38 +0530

    add mapper method to map order to order-response

--- order-service/src/main/java/com/ecom/order/mapper/OrderMapper.java
+++ order-service/src/main/java/com/ecom/order/mapper/OrderMapper.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import com.ecom.order.model.Order;
 import com.ecom.order.request.OrderRequest;
+import com.ecom.order.response.OrderResponse;
 
 @Service
 public class OrderMapper {
@@ -16,5 +17,15 @@ public class OrderMapper {
 				payment(request.payment()).
 				totalAmount(request.amount()).
 				customerId(request.customerId()).build();
+	}
+	
+	public OrderResponse fromOrder(Order order) {
+		return new OrderResponse(
+				order.getId(),
+				order.getReference(),
+				order.getTotalAmount(),
+				order.getPayment(),
+				order.getCustomerId()
+				);
 	}
 }
