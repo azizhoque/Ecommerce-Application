7b1b57c66a1c944a64b6b47e36e70afabeab0753
azizhoque <azizhoque72@gmail.com>
2026-09-24 01:26:46 +0530

    add notification repository

---
+++ notification-service/src/main/java/com/ecom/notification/repository/INotificationRepository.java
@@ -0,0 +1,11 @@
+package com.ecom.notification.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ecom.notification.kafka.payment.PaymentConfirmation;
+
+@Repository
+public interface INotificationRepository extends MongoRepository<PaymentConfirmation, String> {
+
+}
