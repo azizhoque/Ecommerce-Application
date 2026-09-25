22f68ec3044bb78501964794d0fc0280dec7e45f
azizhoque <azizhoque72@gmail.com>
2026-09-25 14:54:32 +0530

    add mapper as a bean

--- customer-service/src/main/java/com/ecom/mapper/CustomerMapper.java
+++ customer-service/src/main/java/com/ecom/mapper/CustomerMapper.java
@@ -1,15 +1,18 @@
 package com.ecom.mapper;
 
+import org.springframework.stereotype.Component;
+
 import com.ecom.customer.request.CustomerRequest;
 import com.ecom.customer.response.CustomerResponse;
 import com.ecom.model.Customer;
 
+@Component
 public class CustomerMapper {
 
 	public Customer toCustomer(CustomerRequest request) {
-		
-		if (request==null) {
-			return null;
+
+		if (request == null) {
+			throw new IllegalArgumentException("CustomerRequest cannot be null or mapper can not be null");
 		}
 		return Customer.builder().
 				firstName(request.firstName()).
@@ -18,12 +21,12 @@ public class CustomerMapper {
 				address(request.address()).
 				build();
 	}
-	
+
 	public CustomerResponse fromCustomer(Customer customer) {
 		return new CustomerResponse(
 				customer.getId(), 
 				customer.getFirstName(), 
-				customer.getLastName(), 
+				customer.getLastName(),
 				customer.getEmail(), 
 				customer.getAddress());
 	}
